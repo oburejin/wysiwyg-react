d70e65dc52b515af82dcc472bc0ac4889261b513
Maksim Oblezin <oburejin@gmail.com>
2017-06-29 21:59:00 +0300

    simple react sortable dnd example

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,23 @@
 import React, {Component} from 'react';
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import ReactDOM from 'react-dom';
+import List from './dnd/list.jsx';
+import Container from './dnd/container.jsx';
+import './s/app.sass';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    
-  }
-
-  
   render() {
    
     return (
-      <div >
-       ...
+      <div>
+        <List/>
       </div>
     )
   }
 
 }
+
 
 ReactDOM.render(<App/>, document.getElementById('app'));
